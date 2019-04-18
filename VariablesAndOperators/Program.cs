aadf9d874d36df44c19b3632a72c65e94e9b8b06
Ryan White <WHITER23@my.erau.edu>
2019-04-18 10:26:33 -0400

    test

---
+++ VariablesAndOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ValueTuple;
+
+namespace VariablesAndOperators
+{
+    class Program
+    {
+        static (int, string) Method()
+        {
+            return (17, "cats");
+        }
+
+        static void Main(string[] args)
+        {
+            var myTuple = Method();
+
+            (var i, var s) = myTuple;
+
+            Console.WriteLine($"{i} {myTuple.cats}");
+        }
+    }
+}
